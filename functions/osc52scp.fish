e272124d5c9a7aa9c7e6f5abfae68a44972cb943
Amos Bird <amosbird@gmail.com>
2017-05-15 10:18:56 +0800

    fix osc52scp.

--- functions/osc52scp.fish
+++ functions/osc52scp.fish
@@ -5,19 +5,16 @@ function osc52scp -d "scp file via osc52" --argument-names 'file'
   end
   set max 74994
   if test -d $file -o -f $file
-
-
     if test (stat -c%s $file) -ge 10000000
       echo "File $file is too large."
       return 1
     end
-
     set path (readlink -f $file)
     set buf "scp "(whoami)"@"(hostname)":"$path" "/tmp/(basename $path)
     set esc "\033]52;y;"(printf $buf | head -c $max | base64 | tr -d '\r\n')"\a"
     set esc "\033Ptmux;\033"$esc"\033\\" # tmux
     printf "$esc"
-    browser "file:///tmp/"(basename $path)
+    osc52browser "file:///tmp/"(basename $path)
   else
     echo "File $file is not an existing file or directory."
     return 1
