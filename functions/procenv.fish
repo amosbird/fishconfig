e26054e416ca14aa4098836ecbf5c3da9fb7f9d0
Amos Bird <amosbird@gmail.com>
2017-05-22 10:25:16 -0800

    fix procenv default val.

--- functions/procenv.fish
+++ functions/procenv.fish
@@ -1,5 +1,5 @@
 function procenv -d "return process env, default self"
-  set -l proc self
+  set -l proc %self
   set -q argv[2]; and set proc $argv[2]
   awk -v p="$argv[1]" 'BEGIN {RS="\0"; ORS="\n"} $0 ~ p' /proc/"$proc"/environ;
 end
