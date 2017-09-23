9cbb99fdf56b334116257156e04eb1a03cfbdcdc
Amos Bird <amosbird@gmail.com>
2017-09-23 23:32:52 +0800

    fix multiline paste (really).

--- functions/__fish_stop_bracketed_paste.fish
+++ functions/__fish_stop_bracketed_paste.fish
@@ -1,8 +1,10 @@
-# Defined in /tmp/fish.nRQOVY/__fish_stop_bracketed_paste.fish @ line 2
+# Defined in /tmp/fish.WsvYc1/__fish_stop_bracketed_paste.fish @ line 2
 function __fish_stop_bracketed_paste
 	# Restore the last bind mode.
-    set fish_bind_mode $__fish_last_bind_mode
-    set -e __fish_paste_quoted
-    commandline -r (printf "%s" (string trim -- (commandline)))
-    commandline -f force-repaint
+  set fish_bind_mode $__fish_last_bind_mode
+  set -e __fish_paste_quoted
+  set -l cmdline (string trim -- (commandline))
+  test -z "$cmdline[-1]"; and set -e cmdline[-1]
+  commandline -r -- $cmdline
+  commandline -f force-repaint
 end
